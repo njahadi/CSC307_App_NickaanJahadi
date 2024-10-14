24f62b039c7029169e73801107c33319aa46505f
njahadi <njahadi@calpoly.edu>
2024-10-13 18:51:43 -0700

    IE3 step 3

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -12,10 +12,6 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
-
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
@@ -29,6 +25,26 @@ function MyApp() {
         console.log(error);
       });
   }, []);
+
+  function postUser(person) {
+    const promise = fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }
+
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   return (
     <div className="container">
